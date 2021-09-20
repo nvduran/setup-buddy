adb4ef391632dfc6ac219a2b9182ff6dea89831c
nvduran <nvduran@gmail.com>
2021-09-19 20:50:41 -0500

    buttons images basic things etc

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  Button,
+  Alert,
+  Platform,
+} from "react-native";
 
 export default function App() {
   const handlePress = () => console.log("booped");
@@ -9,6 +21,26 @@ export default function App() {
       <Text numberOfLines={1} onPress={handlePress}>
         boop
       </Text>
+      <TouchableHighlight onPress={() => console.log("img tapped")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
+      <Button
+        color="red"
+        title="click"
+        onPress={() =>
+          Alert.alert("Cool title", "Cool message", [
+            { text: "btn 1", onPress: () => console.log("btn 1") },
+            { text: "btn 2", onPress: () => console.log("btn 2") },
+          ])
+        }
+      />
+
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -20,5 +52,7 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
+    // padding top if android, otheriwse 0
+    paddingTop: Platform.OS === "android" ? 20 : 0,
   },
 });
